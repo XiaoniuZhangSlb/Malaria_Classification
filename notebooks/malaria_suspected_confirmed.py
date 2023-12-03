e0a43c91d078f2599a5a01dd24d9e54124d47bde
Pablo Chica <pablochica0@gmail.com>
2023-12-03 18:35:35 +0800

    Second draft with Total Suspected cases

--- notebooks/malaria_suspected_confirmed.py
+++ notebooks/malaria_suspected_confirmed.py
@@ -3,28 +3,35 @@ import streamlit as st
 
 
 #LOAD dataset
-suspected_cases_path ="/home/pcarrillo555/folder_lecture/Final_Project_Malaria/Data/Microscopic_exams_suspected_cases.csv"
-confirmed_cases_path ="/home/pcarrillo555/folder_lecture/Final_Project_Malaria/Data/Microscopic_exams_positive_cases.csv"
+total_suspected_cases_path = "/home/pcarrillo555/folder_lecture/Final_Project_Malaria/Data/WHO_malaria_no_microscopic_suspected_cases.csv"
+suspected_cases_path ="/home/pcarrillo555/folder_lecture/Final_Project_Malaria/Data/WHO_malaria_microscopic_exams_suspected_cases.csv"
+confirmed_cases_path ="/home/pcarrillo555/folder_lecture/Final_Project_Malaria/Data/WHO_malaria_microscopic_exams_positive_cases.csv"
 
 
-def load_data(suspected_cases_path, confirmed_cases_path):
+
+def load_data(suspected_cases_path, confirmed_cases_path, total_suspected_cases_path):
     suspected_df = pd.read_csv(suspected_cases_path)
     confirmed_df = pd.read_csv(confirmed_cases_path)
+    total_suspected_df = pd.read_csv(total_suspected_cases_path)  # Load additional suspected cases
 
-    # Rename the 'Value' column for clarity
+    # Rename columns for clarity
     suspected_df.rename(columns={'Value': 'Suspected_Cases'}, inplace=True)
     confirmed_df.rename(columns={'Value': 'Confirmed_Cases'}, inplace=True)
+    total_suspected_df.rename(columns={'Value': 'Total_Suspected_Cases'}, inplace=True)
 
-    # Remove spaces from 'Suspected_Cases' and 'Confirmed_Cases' and convert to float
+    # Remove spaces and convert to float
     suspected_df['Suspected_Cases'] = suspected_df['Suspected_Cases'].replace(r'\s+', '', regex=True).astype(float)
     confirmed_df['Confirmed_Cases'] = confirmed_df['Confirmed_Cases'].replace(r'\s+', '', regex=True).astype(float)
+    total_suspected_df['Total_Suspected_Cases'] = total_suspected_df['Total_Suspected_Cases'].replace(r'\s+', '', regex=True).astype(float)
 
     # Merge the datasets
-    merged_df = pd.merge(suspected_df, confirmed_df, on=['ParentLocation','Location', 'Period'], how='inner')
+    merged_df = pd.merge(suspected_df, confirmed_df, on=['ParentLocation', 'Location', 'Period'], how='inner')
+    merged_df = pd.merge(merged_df, total_suspected_df, on=['ParentLocation', 'Location', 'Period'], how='inner')
 
     # Fill missing values with zeros
     merged_df['Suspected_Cases'].fillna(0, inplace=True)
     merged_df['Confirmed_Cases'].fillna(0, inplace=True)
+    merged_df['Total_Suspected_Cases'].fillna(0, inplace=True)
 
     # Calculate the proportion of confirmed to suspected cases
     merged_df['Proportion'] = merged_df['Confirmed_Cases'] / merged_df['Suspected_Cases']
@@ -34,10 +41,9 @@ def load_data(suspected_cases_path, confirmed_cases_path):
 
     return merged_df
 
+merged_df = load_data(suspected_cases_path, confirmed_cases_path, total_suspected_cases_path)
 
-merged_df = load_data(suspected_cases_path, confirmed_cases_path)
-
-# Title for the web app
+# Streamlit app setup
 st.title('Malaria Case Analysis')
 
 # Dropdown to select a Parent Location
@@ -54,4 +60,4 @@ country_data = parent_location_data[parent_location_data['Location'] == selected
 
 # Display the data
 st.write('Proportion of Confirmed Malaria Cases to Suspected Cases:')
-st.write(country_data[['Period', 'Suspected_Cases', 'Confirmed_Cases', 'Proportion']])
+st.write(country_data[['Period', 'Total_Suspected_Cases','Suspected_Cases', 'Confirmed_Cases',  'Proportion']])
